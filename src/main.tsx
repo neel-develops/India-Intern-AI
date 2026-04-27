8da452094b6c47bd91710027edc6519fc98478fd
neel-develops <neel-develops@users.noreply.github.com>
2026-04-27 14:37:48 +0530

    fix: Add missing AuthProvider wrapper and set BrowserRouter basename for GH Pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { ThemeProvider } from './components/theme-provider';
+import { AuthProvider } from './hooks/use-auth';
 import { Toaster } from './components/ui/toaster';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <App />
-        <Toaster />
-      </ThemeProvider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <AuthProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <App />
+          <Toaster />
+        </ThemeProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
